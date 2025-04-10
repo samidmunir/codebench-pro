8b1fc91689118cefada69dd08fb1510e28bee5dd
Sami Munir <sami.munir2001@gmail.com>
2025-04-10 04:09:31 -0400

    implementing Product model + product routes

--- Learning/mern-product-store/backend/server.js
+++ Learning/mern-product-store/backend/server.js
@@ -1,14 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import { connectDB } from './config/db.js';
+import productRoutes from './routes/product.route.js';
 
 dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('Server is ready!');
-});
+app.use(express.json()); // for parsing application/json
+
+app.use('/api/products', productRoutes);
 
 app.listen(3000, () => {
     connectDB();
