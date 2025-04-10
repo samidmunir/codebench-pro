0e7ee06771640540da9512430d59848bb6bcde77
Sami Munir <sami.munir2001@gmail.com>
2025-04-10 03:07:51 -0400

    setting up MongoDB connection with mongoose

--- Learning/mern-product-store/backend/server.js
+++ Learning/mern-product-store/backend/server.js
@@ -1,7 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config();
 
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send('Server is ready!');
+});
+
 app.listen(3000, () => {
+    connectDB();
     console.log('Server is running on port 3000');
 });
